7a6e15700ca1d8bf44bab1508526e5ad50bff6a7
yoanexposito <yoan.exposito@ufl.edu>
2025-10-03 23:37:20 -0400

    Fix bug in essay average and grading sum, also fixed F tests by inputing correct value

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -39,9 +39,9 @@ func TestGetGradeF(t *testing.T) {
 
 	gradeCalculator := NewGradeCalculator()
 
-	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
-	gradeCalculator.AddGrade("exam 1", 95, Exam)
-	gradeCalculator.AddGrade("essay on ai ethics", 91, Essay)
+	gradeCalculator.AddGrade("open source assignment", 23, Assignment)
+	gradeCalculator.AddGrade("exam 1", 5, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 12, Essay)
 
 	actual_value := gradeCalculator.GetFinalGrade()
 
